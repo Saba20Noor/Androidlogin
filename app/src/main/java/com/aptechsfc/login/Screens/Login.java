75cd312d84721059246dd47b2efaa304abb48874
Saba <saba.noorahmed12@gmail.com>
2022-11-14 13:08:09 +0500

    login commit

---
+++ app/src/main/java/com/aptechsfc/login/Screens/Login.java
@@ -0,0 +1,16 @@
+package com.aptechsfc.login.Screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.aptechsfc.login.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/aptechsfc/login/Screens/SignUp.java
@@ -0,0 +1,16 @@
+package com.aptechsfc.login.Screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.aptechsfc.login.R;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
